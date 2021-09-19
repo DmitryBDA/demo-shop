9dad7ca9ba8d3dac88c5b83c243f1d27bab9419e
dmitriy <da_belousov@list.ru>
2021-09-19 13:00:21 +0300

    Разбитие шаблона user на partial

---
+++ resources/views/layouts/user/partial/script.blade.php
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="user/assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="user/assets/js/tether.min.js"></script>
+<script type="text/javascript" src="user/assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="user/assets/js/jquery-migrate.min.js"></script>
+<script type="text/javascript" src="user/assets/js/hidemaxlistitem.min.js"></script>
+<script type="text/javascript" src="user/assets/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="user/assets/js/hidemaxlistitem.min.js"></script>
+<script type="text/javascript" src="user/assets/js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="user/assets/js/scrollup.min.js"></script>
+<script type="text/javascript" src="user/assets/js/jquery.waypoints.min.js"></script>
+<script type="text/javascript" src="user/assets/js/waypoints-sticky.min.js"></script>
+<script type="text/javascript" src="user/assets/js/pace.min.js"></script>
+<script type="text/javascript" src="user/assets/js/slick.min.js"></script>
+<script type="text/javascript" src="user/assets/js/scripts.js"></script>
---
+++ resources/views/layouts/user/partial/style.blade.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="user/assets/css/bootstrap.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/font-awesome.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/bootstrap-grid.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/bootstrap-reboot.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/font-techmarket.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/slick.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/techmarket-font-awesome.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/slick-style.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/animate.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/style.css" media="all" />
+<link rel="stylesheet" type="text/css" href="user/assets/css/colors/blue.css" media="all" />
+
+<link rel="stylesheet" type="text/css" href="{{mix('/assets/css/main.css')}}" media="all"/>
+
+<link rel="shortcut icon" href="user/assets/images/fav-icon.png">
