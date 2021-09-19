fee9d17396b036b733ba7014e79757f38dcfcfcd
dmitriy <da_belousov@list.ru>
2021-09-19 12:48:01 +0300

    Создание ролей seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
       $this->call(CategoriesTableSeeder::class);
       Product::factory()->count(100)->create();
+      $this->call(RoleSeeder::class);
     }
 }
