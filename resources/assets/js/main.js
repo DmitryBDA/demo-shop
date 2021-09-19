8b064446c3d7d097b7d4f65c072e5e07446a7c83
dmitriy <da_belousov@list.ru>
2021-09-19 05:42:34 +0300

    сортировка списока продуктов в админке

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -3,12 +3,17 @@ import svg4everybody from 'svg4everybody';*/
 
 $('._input_search').on("input", (elem) => {
   let searchFields = $(elem.target).val();
+  const url = new URL(window.location);
+  let page = url.searchParams.get('page');
+  let sortField = url.searchParams.get('sortField');
 
   $.ajax({
     url: "/admin/products",
     type: "GET",
     data: {
       searchFields: searchFields,
+      page: page,
+      sortField: sortField,
     },
     headers: {
       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -16,9 +21,34 @@ $('._input_search').on("input", (elem) => {
     success: (data) => {
 
         $('._ajax_product_list').html(data)
-        const url = new URL(window.location);
+
         url.searchParams.set('searchFields', searchFields);
         history.pushState(null, null, url);
+    }
+  })
+});
+$(document).on('click', '.sorting', (elem) => {
+  let sortField = $(elem.target).attr('data-sorting').trim();
+  const url = new URL(window.location);
+  let searchFields = url.searchParams.get('searchFields');
+  let page = url.searchParams.get('page');
+
+  $.ajax({
+    url: "/admin/products",
+    type: "GET",
+    data: {
+      sortField: sortField,
+      searchFields: searchFields,
+      page: page,
+    },
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    },
+    success: (data) => {
+
+      $('._ajax_product_list').html(data)
+      url.searchParams.set('sortField', sortField);
+      history.pushState(null, null, url);
     }
   })
 });
