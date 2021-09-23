36caf97153abb532f4194a351e6c217ebd928e0f
dmitriy <da_belousov@list.ru>
2021-09-23 16:35:33 +0300

    Подключение страницы корзина, вывод содержимого списком

--- app/Http/Controllers/Shop/User/CartController.php
+++ app/Http/Controllers/Shop/User/CartController.php
@@ -9,10 +9,9 @@ class CartController extends Controller
 {
   public function cartList()
   {
-    $cartItems = \Cart::getContent();
-    dd($cartItems);
+    $obCartList = \Cart::getContent();
     // dd($cartItems);
-    return view('cart', compact('cartItems'));
+    return view('pages.user.cart', compact('obCartList'));
   }
 
 
